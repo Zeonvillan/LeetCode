beb315d55e39ab7672bb53d4ea54837a6b615666
Mavin Meshram <39011679+Zeonvillan@users.noreply.github.com>
2025-08-22 21:39:28 +0530

    Add files via upload

---
+++ Merge Strings Alternately/Solution.java
@@ -0,0 +1,8 @@
+public class Solution {
+    public static void main(String[] args) {
+        MergeStringsAlternately merger = new MergeStringsAlternately();
+        String word1 = "ab";
+        String word2 = "pqrs";
+        System.out.println(merger.mergeAlternately(word1, word2));
+    }
+}
